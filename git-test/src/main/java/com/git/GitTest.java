70c50551dd9f9d51a93e597995130de3e47d0f4d
kieeber <1784588062@qq.com>
2022-07-21 10:17:41 +0800

    branch01 modified01

--- git-test/src/main/java/com/git/GitTest.java
+++ git-test/src/main/java/com/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("gitTest01");
         System.out.println("gitTest02");
+        System.out.println("Branch01 modified 01");
     }
 }
